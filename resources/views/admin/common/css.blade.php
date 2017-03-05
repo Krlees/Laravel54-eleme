0af2a1dcf3d7ede042d41a5d8e9a32cb8365a1d8
Krlees <515961601@qq.com>
2017-03-05 22:52:35 +0800

    新增table组件

---
+++ resources/views/admin/common/css.blade.php
@@ -0,0 +1,5 @@
+<link href="{{asset('admin/css/bootstrap.min.css?v=3.3.6')}}" rel="stylesheet">
+<link href="{{asset('admin/css/font-awesome.min.css?v=4.4.0')}}" rel="stylesheet">
+<link href="{{asset('admin/css/animate.css')}}" rel="stylesheet">
+<link href="{{asset('admin/css/style.css?v=4.1.0')}}" rel="stylesheet">
+<link href="{{asset('admin/css/other.css')}}" rel="stylesheet">
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -13,11 +13,8 @@
 
     <link rel="shortcut icon" href="favicon.ico">
 
-    <link href="{{asset('admin/css/bootstrap.min.css?v=3.3.6')}}" rel="stylesheet">
-    <link href="{{asset('admin/css/font-awesome.min.css?v=4.4.0')}}" rel="stylesheet">
-    <link href="{{asset('admin/css/animate.css')}}" rel="stylesheet">
-    <link href="{{asset('admin/css/style.css?v=4.1.0')}}" rel="stylesheet">
-    <link href="{{asset('admin/css/other.css')}}" rel="stylesheet">
+    @include('admin/common/css')
+
     @yield('css')
 
 </head>
@@ -31,6 +28,7 @@
 <script src="{{asset('admin/js/content.js?v=1.0.0')}}"></script>
 <script src="{{asset('admin/js/plugins/layer/layer.min.js')}}"></script>
 
+@include('admin/common/js')
 
 @yield('js')
 
